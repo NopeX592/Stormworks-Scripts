0885c63778f57f179e498511e354a43eafca5642
NopeX592 <58185658+NopeX592@users.noreply.github.com>
2022-03-21 02:14:35 +0100

    Did cool things

--- Map Autopilot/Map_Autopilot p1.lua
+++ Map Autopilot/Map_Autopilot p1.lua
@@ -1,5 +1,7 @@
 zoo = 5
 zoom = -1.431
+x = 0
+y = 0
 button_size = 6
 
 function onTick()
@@ -9,59 +11,65 @@ function onTick()
 
     worldX = input.getNumber(5)
     worldY = input.getNumber(6)
-    x = input.getNumber(7)
-    y = input.getNumber(8)
-    zoom = input.getNumber(9)
+
+    output.setBool(4, false)
 
 	--Map Snap Controls
 	isPressingStart = isPressed and isPntInRect(inputX, inputY, 7, 43, button_size, button_size)
 	isPressingCenter = isPressed and isPntInRect(inputX, inputY, 7, 55, button_size, button_size)
 
-    output.setBool(1, isPressingStart)
-    output.setBool(2, isPressingCenter)
+    --Center
+	if isPressingCenter then
+        x = 0
+        y = 0
+    end
+
+    --Zoom
+	isPressingZoomIn = isPressed and isPntInRect(inputX, inputY, 58, 5, button_size, button_size)
+	isPressingZoomOut = isPressed and isPntInRect(inputX, inputY, 58, 11, button_size, button_size)
+	
+	if isPressingZoomIn then
+		zoom = zoom + 0.01
+	elseif isPressingZoomOut then
+		zoom = zoom - 0.01
+	end
+    if zoom < -1.431 then
+        zoom = -1.431
+    end
+    if zoom > 2.431 then
+        zoom = 2.431
+    end
+	
+    --Panning
+	isPressingXPlus = isPressed and isPntInRect(inputX, inputY, 58, 49, button_size, button_size)
+	isPressingXMinus = isPressed and isPntInRect(inputX, inputY, 46, 49, button_size, button_size)
+	isPressingYPlus = isPressed and isPntInRect(inputX, inputY, 52, 43, button_size, button_size)
+	isPressingYMinus = isPressed and isPntInRect(inputX, inputY, 52, 55, button_size, button_size)
+
+    if isPressingXPlus then
+        x = x + 0.75*(zoo^(zoom + 1.431))
+    elseif isPressingXMinus then
+        x = x - 0.75*(zoo^(zoom + 1.431))
+    end
+
+    if isPressingYPlus then
+        y = y + 0.75*(zoo^(zoom + 1.431))
+    elseif isPressingYMinus then
+        y = y - 0.75*(zoo^(zoom + 1.431))
+    end
+
+    --Set Outputs
+    output.setNumber(7, x)
+    output.setNumber(8, y)
+    output.setNumber(9, zoom)
+
+    output.setBool(3, isPressingStart)
+
+    if not (isPressingStart or isPressingCenter or isPressingZoomIn or isPressingZoomOut or isPressingXPlus or isPressingXMinus or isPressingYPlus or isPressingYMinus) then
+        output.setBool(4, true)
+    end
 end
 
 function isPntInRect(x, y, rectX, rectY, rectW, rectH)
 	return x > rectX and y > rectY and x < rectX+rectW and y < rectY+rectH
-end
-
-function onDraw()
-	w = screen.getWidth()
-	h = screen.getHeight()
-	cw = w/2
-    ch = h/2
-    
-	--Draw Map	
-	screen.setMapColorOcean(14,46,48)
-	screen.setMapColorShallows(24,68,72)
-	screen.setMapColorLand(90,90,90)
-	screen.setMapColorGrass(64,85,48)
-	screen.setMapColorSand(100,93,41)
-	screen.setMapColorSnow(200,200,200)
-	screen.drawMap(x+worldX, y+worldY, zoo^zoom)
-    pixelX, pixelY = map.mapToScreen(x+worldX, y+worldY, zoo^zoom, 64, 64, worldX, worldY)
-    
-    --Draw Map Control Buttons
-    screen.setColor(0,0,0)
-    screen.drawTextBox(7, 43, button_size, button_size, "S", 1, 1)
-    screen.drawTextBox(7, 55, button_size, button_size, "R", 1, 1)
-    screen.drawRect(7, 43, button_size, button_size)
-    screen.drawRect(7, 55, button_size, button_size)
-
-    screen.setColor(200,20,20,150)
-    if isPressingStart then
-        screen.drawRectF(7, 43, button_size+1, button_size+1)
-    end
-    if isPressingCenter then
-        screen.drawRectF(7, 55, button_size+1, button_size+1)
-    end
-
-    --Draw Coordinates
-    screen.setColor(0,0,0)
-    screen.drawTextBox(2, 1, 28, button_size, math.floor(worldX), -1, 0)
-    screen.drawTextBox(34, 1, 28, button_size, math.floor(worldX), -1, 0)
-
-    --blocker rectangle
-    screen.setColor(0,0,0)
-    screen.drawRectF(w/3*2+1, 0, w/3, h)
 end
--- Map Autopilot/Map_Autopilot p2.lua
+++ Map Autopilot/Map_Autopilot p2.lua
@@ -1,66 +1,24 @@
 zoo = 5
-zoom = -1.431
-x = 0
-y = 0
 button_size = 6
 
 function onTick()
-	inputX = input.getNumber(3)
+    inputX = input.getNumber(3)
 	inputY = input.getNumber(4)
-	isPressed = input.getBool(1)
-    isPressingCenter = input.getBool(2)
+    isPressed = input.getBool(1)
+    buttonPressed = input.getBool(4)
 
     worldX = input.getNumber(5)
     worldY = input.getNumber(6)
+    x = input.getNumber(7)
+    y = input.getNumber(8)
+    zoom = input.getNumber(9)
+    pixelX, pixelY = map.mapToScreen(x+worldX, y+worldY, zoo^zoom, 64, 96, worldX, worldY)
 
-    --Center
-	if isPressingCenter then
-        x = 0
-        y = 0
+    if isPressed and buttonPressed then
+        worldWPX, worldWPY = map.screenToMap(inputX, inputY, zoo^zoom, 64, 96, pixelX, pixelY)    
+        output.setNumber(9, worldWPX)
+        output.setNumber(10, worldWPY)
     end
-
-    --Zoom
-	isPressingZoomIn = isPressed and isPntInRect(inputX, inputY, 58, 5, button_size, button_size)
-	isPressingZoomOut = isPressed and isPntInRect(inputX, inputY, 58, 11, button_size, button_size)
-	
-	if isPressingZoomIn then
-		zoom = zoom + 0.01
-	elseif isPressingZoomOut then
-		zoom = zoom - 0.01
-	end
-    if zoom < -1.431 then
-        zoom = -1.431
-    end
-    if zoom > 2.431 then
-        zoom = 2.431
-    end
-	
-    --Panning
-	isPressingXPlus = isPressed and isPntInRect(inputX, inputY, 58, 49, button_size, button_size)
-	isPressingXMinus = isPressed and isPntInRect(inputX, inputY, 46, 49, button_size, button_size)
-	isPressingYPlus = isPressed and isPntInRect(inputX, inputY, 52, 43, button_size, button_size)
-	isPressingYMinus = isPressed and isPntInRect(inputX, inputY, 52, 55, button_size, button_size)
-
-    if isPressingXPlus then
-        x = x + 0.75*(zoo^(zoom + 1.431))
-    elseif isPressingXMinus then
-        x = x - 0.75*(zoo^(zoom + 1.431))
-    end
-
-    if isPressingYPlus then
-        y = y + 0.75*(zoo^(zoom + 1.431))
-    elseif isPressingYMinus then
-        y = y - 0.75*(zoo^(zoom + 1.431))
-    end
-
-    --set outputs
-    output.setNumber(7, x)
-    output.setNumber(8, y)
-    output.setNumber(9, zoom)
-end
-
-function isPntInRect(x, y, rectX, rectY, rectW, rectH)
-	return x > rectX and y > rectY and x < rectX+rectW and y < rectY+rectH
 end
 
 function onDraw()
@@ -68,15 +26,28 @@ function onDraw()
 	h = screen.getHeight()
 	cw = w/2
     ch = h/2
+    
+	--Draw Map	
+	screen.setMapColorOcean(14,46,48)
+	screen.setMapColorShallows(24,68,72)
+	screen.setMapColorLand(90,90,90)
+	screen.setMapColorGrass(64,85,48)
+	screen.setMapColorSand(100,93,41)
+	screen.setMapColorSnow(200,200,200)
+	screen.drawMap(x+worldX, y+worldY, zoo^zoom)
 
     --Draw Player
-    pixelX, pixelY = map.mapToScreen(x+worldX, y+worldY, zoo^zoom, 64, 64, worldX, worldY)
     screen.setColor(200, 20, 20)
     screen.drawRect(pixelX, pixelY, 2, 2)
     screen.setColor(250, 10, 10, 50)
     screen.drawRect(pixelX-1, pixelY-1, 4, 4)
     screen.setColor(255, 255, 255, 100)
     screen.drawCircleF(pixelX+1, pixelY+1, 1)
+
+    --Draw Coordinates
+    screen.setColor(0,0,0)
+    screen.drawTextBox(2, 1, 28, button_size, math.floor(worldX), -1, 0)
+    screen.drawTextBox(34, 1, 28, button_size, math.floor(worldX), -1, 0)
 
     --Draw Zoom
     screen.setColor(0,0,0)
@@ -88,15 +59,7 @@ function onDraw()
     screen.drawLine(61, 20, 61, 42)
     
     screen.setColor(200,20,20)
-    screen.drawRectF(58, (zoo^zoom * 19/50) + 20, 7, 3)
-
-    screen.setColor(200,20,20,150)
-    if isPressingZoomIn then
-        screen.drawRectF(58, 5, button_size+1, button_size+1)
-    end
-    if isPressingZoomOut then
-        screen.drawRectF(58, 11, button_size+1, button_size+1)
-    end
+    screen.drawRectF(59, (zoo^zoom * 19/50) + 20, 5, 3)
 
     --Draw Panning Buttons
     screen.setColor(0,0,0)
@@ -109,17 +72,14 @@ function onDraw()
     screen.drawRect(52, 43, button_size, button_size)
     screen.drawRect(52, 55, button_size, button_size)
 
-    screen.setColor(200,20,20,150)
-    if isPressingXPlus then
-        screen.drawRectF(58, 49, button_size+1, button_size+1)
-    end
-    if isPressingXMinus then
-        screen.drawRectF(46, 49, button_size+1, button_size+1)
-    end
-    if isPressingYPlus then
-        screen.drawRectF(52, 43, button_size+1, button_size+1)
-    end
-    if isPressingYMinus then
-        screen.drawRectF(52, 55, button_size+1, button_size+1)
-    end
+    --Draw Map Control Buttons
+    screen.setColor(0,0,0)
+    screen.drawTextBox(7, 43, button_size, button_size, "S", 1, 1)
+    screen.drawTextBox(7, 55, button_size, button_size, "R", 1, 1)
+    screen.drawRect(7, 43, button_size, button_size)
+    screen.drawRect(7, 55, button_size, button_size)
+    
+    --blocker rectangle
+    screen.setColor(0,0,0)
+    screen.drawRectF(w/3*2+1, 0, w/3, h)
 end
